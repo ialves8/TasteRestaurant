dee8e8704100c2d3bca15072fc58568aa6e7d5e5
Iranildo Ferreira Alves <ialves8@hotmail.com>
2019-02-05 15:07:52 -0200

    Adicionado _IndividualButtonPartial ao projeto

---
+++ TasteRestaurant/Data/CategoryType.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TasteRestaurant.Data
+{
+    public class CategoryType
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Nome")]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Ordem")]
+        public int DisplayOrder { get; set; }
+    }
+}
