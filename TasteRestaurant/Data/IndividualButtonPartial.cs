4610c99f8d9ec669bc8fc31ddbc8a7f7a9558121
Iranildo Ferreira Alves <ialves8@hotmail.com>
2019-02-05 15:36:42 -0200

    Adicionado _TableButtonPartial ao projeto

---
+++ TasteRestaurant/Data/IndividualButtonPartial.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TasteRestaurant.Data
+{
+    public class IndividualButtonPartial
+    {
+        public string Page { get; set; }
+        public string Glyph { get; set; }
+        public string ButtonType { get; set; }
+
+        public int? Id { get; set; }
+
+        public string ActionParameter
+        {
+            get
+            {
+                if (Id != 0 && Id != null)
+                {
+                    return Id.ToString();
+                }
+                return null;
+            }
+        }
+
+    }
+}
