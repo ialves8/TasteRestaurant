55a6dfd956713f9af6718294d3b1867467a06f7a
Iranildo Ferreira Alves <ialves8@hotmail.com>
2019-02-06 23:42:10 -0200

    Adicionado Details e Delete Razor Page ao projeto em Tipos de Categoria.

---
+++ TasteRestaurant/Pages/CategoryTypes/Delete.cshtml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using TasteRestaurant.Data;
+
+namespace TasteRestaurant.Pages.CategoryTypes
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        public DeleteModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [BindProperty]
+        public CategoryType CategoryType { get; set; }
+
+        public async Task<IActionResult> OnGet(int? id)
+        {
+            if (id==null)
+            {
+                return NotFound();
+            }
+
+            CategoryType = await _db.CategoryTypes.SingleOrDefaultAsync(c=>c.Id==id);
+
+            if (CategoryType==null)
+            {
+                return NotFound();
+            }
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            CategoryType = await _db.CategoryTypes.FindAsync(id);
+
+            if (CategoryType != null)
+            {
+                _db.CategoryTypes.Remove(CategoryType);
+                await _db.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
