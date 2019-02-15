75db6171f0b73df13ddba752a14d8df26a421c98
Iranildo Ferreira Alves <ialves8@hotmail.com>
2019-02-15 16:23:51 -0200

    Adicionado ShoppingCart (Carrinho de compras) ao projeto

--- TasteRestaurant/Data/Migrations/20190215182101_addShoppingCartToDatabase.Designer.cs
+++ TasteRestaurant/Data/Migrations/20190215182101_addShoppingCartToDatabase.Designer.cs
@@ -11,9 +11,10 @@ using TasteRestaurant.Data;
 namespace TasteRestaurant.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190215182101_addShoppingCartToDatabase")]
+    partial class addShoppingCartToDatabase
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
